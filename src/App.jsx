f36d53579672e54805167a2624d8335f8566641e
maruti-panchal <panchal.maruti70@gmail.com>
2024-05-28 14:42:09 +0530

    installed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Home from "./pages/Home";
+
 function App() {
-  const x = 23;
-  return <>World Wise</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
